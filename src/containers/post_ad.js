1cf2e13758a6103bf682ed085309b94f8b5f6e04
Sagar Dutta <sagar_dutta@intuit.com>
2016-03-02 19:56:52 -0600

    Adding react-router to the mix

---
+++ src/containers/post_ad.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class PostNewAd extends Component{
+
+
+  render(){
+    return(
+      <div>"Post new ad"</div>
+    );
+  }
+}
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Route, IndexRoute} from 'react-router';
+
+import App from './components/app';
+import SearchPage from './containers/search_page';
+import PostAd from './containers/post_ad';
+
+
+export default(
+  <Route path='/' component={App} >
+    <IndexRoute component={SearchPage} />
+     <Route path='/posts/new' component={PostAd} />
+  </Route>
+);
