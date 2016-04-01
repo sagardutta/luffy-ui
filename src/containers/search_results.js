21377cbc26294ab812dc9200b5497be2056d932b
Sagar Dutta <sagar_dutta@intuit.com>
2016-04-01 09:23:06 -0500

    opening a modal window when clicked on a row

--- src/containers/search_results.js
+++ src/containers/search_results.js
@@ -1,96 +1,241 @@
-import React, {Component, PropTypes} from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {editEntry} from '../actions/index';
+import React, {
+  Component,
+  PropTypes
+} from 'react';
+import {
+  connect
+} from 'react-redux';
+import {
+  bindActionCreators
+} from 'redux';
+import {
+  editEntry
+} from '../actions/index';
 import Time from 'react-time';
+import Modal from 'react-modal';
 
 
-class SearchResults extends Component{
-constructor(props){
-  super(props);
-  this.editEntry = this.editEntry.bind(this);
-  this.renderResult = this.renderResult.bind(this);
-}
+class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+    this.editEntry = this.editEntry.bind(this);
+    this.renderResult = this.renderResult.bind(this);
+    this.displayRow = this.displayRow.bind(this);
+    this.renderModal = this.renderModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.state = {
+      modalIsOpen: false,
+      result: {}
+    }
 
-static contextTypes = {
+  }
+
+  static contextTypes = {
     router: PropTypes.object
-};
+  };
 
-editEntry(id){
-  this.props.editEntry(id).then(() =>{
-      this.context.router.push('posts/update');
-  });
-}
-  renderResult(result){
-    var tags = result.tags.map(function(tag){
-      return(<span key={tag}>{tag} </span>);
+  closeModal() {
+    this.setState({
+      modalIsOpen: false
     });
+  }
+
+  editEntry(id) {
+    this.props.editEntry(id).then(() => {
+      this.context.router.push('posts/update');
+    });
+  }
+
+  displayRow(result) {
+    console.log(result._id);
+    this.setState({
+      modalIsOpen: true,
+      result: result
+    });
+
+  }
+
+  renderModal() {
+
     return (
+      < Modal isOpen = {
+        this.state.modalIsOpen
+      }
+      onRequestClose = {
+        this.closeModal
+      } >
+
+
+
+      < h1 > {
+        this.state.result._id
+      } < /h1>
+
+
+      < button onClick = {
+        this.closeModal
+      } > close < /button>
+
+      < /Modal>
+    );
+
+  }
+
+
+  renderResult(result) {
+    var tags = result.tags.map(function(tag) {
+        return ( < span key = {
+            tag
+          } > {
+            tag
+          } < /span>);
+        });
+      return (
+
+
+
         // <div  className="col-md-4" key={result._id}>
-        <tr key={result._id}>
+        < tr key = {
+          result._id
+        }
+        onClick = {
+          () => this.displayRow(result)
+        } >
 
-<td>{result.minAge}</td>
-<td>{result.maxAge}</td>
-<td>{result.applicationProcess}</td>
-<td>{result.selectionProcess}</td>
-<td>{result.source}</td>
-<td><Time value={result.lastDate} format="YYYY/MM/DD" /></td>
-<td><Time value={result.notificationDate} format="YYYY/MM/DD" /></td>
-<td><a href={result.linkToSource}>{result.linkToSource}</a></td>
-<td>{result.contactDetails}</td>
-<td> {tags}</td>
-<td onClick={() => this.editEntry(result._id)}> <i className="fa fa-pencil"></i> </td>
+        < td > {
+          result.minAge
+        } < /td> < td > {
+          result.maxAge
+        } < /td> < td > {
+          result.applicationProcess
+        } < /td> < td > {
+          result.selectionProcess
+        } < /td> < td > {
+          result.source
+        } < /td> < td > < Time value = {
+          result.lastDate
+        }
+        format = "YYYY/MM/DD" / > < /td> < td > < Time value = {
+          result.notificationDate
+        }
+        format = "YYYY/MM/DD" / > < /td> < td > < a href = {
+          result.linkToSource
+        } > {
+          result.linkToSource
+        } < /a></td >
+        < td > {
+          result.contactDetails
+        } < /td> < td > {
+          tags
+        } < /td> < td onClick = {
+          () => this.editEntry(result._id)
+        } > < i className = "fa fa-pencil" > < /i> </td >
 
 
 
-      </tr>
-        // </div>
-    );
-  }
-  render(){
+        < /tr>
 
-    if(this.props.searchResults.docs === undefined || this.props.searchResults.docs.length == 0 ){
-      return(
-        <div> No Search results to display</div>
+
       );
-    }else{
+    }
+    render() {
 
-    return(
+      if (this.props.searchResults.docs === undefined || this.props.searchResults.docs.length == 0) {
+        return ( < div > No Search results to display < /div>);
+      } else {
+
+        return (
+
+
+          < div >
+
+          < table className = "table table-striped small" >
+          < thead >
+          < tr >
+          < th > Min Age < /th> < th > Max Age < /th> < th > Application Process < /th> < th > Selection Process < /th> < th > Source < /th> < th > Last Date < /th> < th > Notification Date < /th> < th > Link To Source < /th> < th > Contact Details < /th> < th > Tags < /th> < th > edit < /th> < /tr> < /thead> < tbody > {
+            this.props.searchResults.docs.map(this.renderResult)
+          } < /tbody> < /table>
 
 
 
-        <table className="table table-striped small">
-<thead>
-<tr>
-  <th>Min Age</th>
-  <th>Max Age</th>
-  <th>Application Process</th>
-  <th>Selection Process</th>
-  <th>Source</th>
-  <th>Last Date</th>
-  <th>Notification Date</th>
-  <th>Link To Source</th>
-  <th>Contact Details</th>
-  <th>Tags</th>
-  <th>edit</th>
-  </tr>
-</thead>
-<tbody >
-          {this.props.searchResults.docs.map(this.renderResult)}
-          </tbody>
-          </table>
+          < Modal isOpen = {
+            this.state.modalIsOpen
+          }
+          onRequestClose = {
+            this.closeModal
+          } >
 
-    );
+
+
+          < h1 > Admission Details < /h1>
+
+          < table className = "table table-bordered small" >
+          < tbody >
+          < tr >
+          < td > Min Age < /td> < td > {
+            this.state.result.minAge
+          } < /td> < /tr>
+
+          < tr >
+          < td > Max Age < /td> < td > {
+            this.state.result.maxAge
+          } < /td> < /tr>
+
+          < tr >
+          < td > Application Process < /td> < td > {
+            this.state.result.applicationProcess
+          } < /td> < /tr>
+
+          < tr >
+          < td > Selection Process < /td> < td > {
+            this.state.result.selectionProcess
+          } < /td> < /tr>
+
+          < tr >
+          < td > Source < /td> < td > {
+            this.state.result.source
+          } < /td> < /tr>
+
+          < tr >
+          < td > Last Date < /td> < td > < Time value = {
+            this.state.result.lastDate
+          }
+          format = "YYYY/MM/DD" / > < /td> < /tr>
+
+          < tr >
+          < td > link to source < /td> < td > {
+            this.state.result.linkToSource
+          } < /td> < /tr>
+
+          < /tbody> < /table>
+
+
+          < button onClick = {
+            this.closeModal
+          } > close < /button>
+
+          < /Modal>
+
+          < /div>
+
+        );
+      }
+    }
   }
-}
-}
 
-function mapStateToProps({searchResults}){
-  return {searchResults,editEntry};
-}
+  function mapStateToProps({
+    searchResults
+  }) {
+    return {
+      searchResults,
+      editEntry
+    };
+  }
 
-function mapDispatchToProps(dispatch){
-  return bindActionCreators({editEntry}, dispatch);
-}
+  function mapDispatchToProps(dispatch) {
+    return bindActionCreators({
+      editEntry
+    }, dispatch);
+  }
 
-export default connect (mapStateToProps, mapDispatchToProps)(SearchResults);
+  export default connect(mapStateToProps, mapDispatchToProps)(SearchResults);
--- src/containers/update_admission.js
+++ src/containers/update_admission.js
@@ -1,38 +1,59 @@
-import React, {Component, PropTypes} from 'react';
-import {reduxForm} from 'redux-form';
-import {updateAdmission, deleteAdmission} from '../actions/index';
-import {Link} from 'react-router';
+import React, {
+  Component,
+  PropTypes
+} from 'react';
+import {
+  reduxForm
+} from 'redux-form';
+import {
+  updateAdmission,
+  deleteAdmission
+} from '../actions/index';
+import {
+  Link
+} from 'react-router';
 
-export const fields = ['minAge','maxAge','applicationProcess','selectionProcess','source','lastDate','notificationDate','linkToSource','contactDetails','tags'];
+export const fields = ['minAge', 'maxAge', 'applicationProcess', 'selectionProcess', 'source', 'lastDate', 'notificationDate', 'linkToSource', 'contactDetails', 'tags'];
 
- class UpdateAdmission extends Component{
-   static contextTypes ={
-     router: PropTypes.object
-   }
-   onSubmit(props){
+class UpdateAdmission extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+  onSubmit(props) {
 
-     if( props.tags.constructor === String ){
-        props.tags = props.tags.split(',');
-     }
+    if (props.tags.constructor === String) {
+      props.tags = props.tags.split(',');
+    }
 
-      this.props.updateAdmission(this.props._id,props).then(() => {
-        this.context.router.push('/');
-      });
-   }
+    this.props.updateAdmission(this.props._id, props).then(() => {
+      this.context.router.push('/');
+    });
+  }
 
-   onDelete(){
-     console.log("onDelete");
-      this.props.deleteAdmission(this.props._id).then(() => {
-        this.context.router.push('/');
-      });
-   }
+  onDelete() {
+    console.log("onDelete");
+    this.props.deleteAdmission(this.props._id).then(() => {
+      this.context.router.push('/');
+    });
+  }
 
 
 
-  render(){
+  render() {
 
-    const{
-      fields :{minAge,maxAge,applicationProcess,selectionProcess,source,lastDate,notificationDate,linkToSource,contactDetails, tags},
+    const {
+      fields: {
+        minAge,
+        maxAge,
+        applicationProcess,
+        selectionProcess,
+        source,
+        lastDate,
+        notificationDate,
+        linkToSource,
+        contactDetails,
+        tags
+      },
       handleSubmit,
       resetForm,
       submitting,
@@ -41,76 +62,92 @@ export const fields = ['minAge','maxAge','applicationProcess','selectionProcess'
 
     console.log(initialValues);
 
-    return(
-      <div className="m-t-3" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-        <h2> Update an admission entry </h2>
-      <form role="form" className="p-t-3" >
+    return ( < div className = "m-t-3"
+      onSubmit = {
+        handleSubmit(this.onSubmit.bind(this))
+      } >
+      < h2 > Update an admission entry < /h2> < form role = "form"
+      className = "p-t-3" >
 
 
-  <div className="form-group"><label>Min Age</label> <div><input className="form-control" type="text" placeholder="Min Age" {...minAge}/></div></div>
-  <div className="form-group"><label>Max Age</label><div> <input className="form-control" type="text" placeholder="Max Age" {...maxAge}/></div></div>
-  <div className="form-group"><label>Application Process</label><div> <input className="form-control" type="text" placeholder="Application Process" {...applicationProcess}/></div></div>
-  <div className="form-group"><label>Selection Process</label><div> <input className="form-control" type="text" placeholder="Selection Process" {...selectionProcess}/></div></div>
-  <div className="form-group"><label>Source</label><div> <input className="form-control" type="text" placeholder="Source" {...source}/></div></div>
-  <div className="form-group"><label>Last Date</label><div> <input className="form-control" type="date" placeholder="Last Date" {...lastDate}/></div></div>
-  <div className="form-group"><label>Notification Date</label><div> <input className="form-control" type="date" placeholder="Notification Date" {...notificationDate}/></div></div>
-  <div className="form-group"><label>Link ToSource</label><div> <input className="form-control" type="text" placeholder="Link ToSource" {...linkToSource}/></div></div>
-  <div className="form-group"><label>Contact Details</label><div> <input className="form-control" type="text" placeholder="Contact Details" {...contactDetails}/></div></div>
-  <div className="form-group">
-  <label>Tags</label><div> <input className={`form-control ${tags.touched && tags.error ? ' has-danger' : ''}`} type="text"  placeholder="tags" {...tags}/></div>
-  <div className="text-help">
-    {tags.error}
-  </div>
-  </div>
+      < div className = "form-group" > < label > Min Age < /label> <div><input className="form-control" type="text" placeholder="Min Age" {...minAge}/ > < /div></div >
+      < div className = "form-group" > < label > Max Age < /label><div> <input className="form-control" type="text" placeholder="Max Age" {...maxAge}/ > < /div></div >
+      < div className = "form-group" > < label > Application Process < /label><div> <input className="form-control" type="text" placeholder="Application Process" {...applicationProcess}/ > < /div></div >
+      < div className = "form-group" > < label > Selection Process < /label><div> <input className="form-control" type="text" placeholder="Selection Process" {...selectionProcess}/ > < /div></div >
+      < div className = "form-group" > < label > Source < /label><div> <input className="form-control" type="text" placeholder="Source" {...source}/ > < /div></div >
+      < div className = "form-group" > < label > Last Date < /label><div> <input className="form-control" type="date" placeholder="Last Date" {...lastDate}/ > < /div></div >
+      < div className = "form-group" > < label > Notification Date < /label><div> <input className="form-control" type="date" placeholder="Notification Date" {...notificationDate}/ > < /div></div >
+      < div className = "form-group" > < label > Link ToSource < /label><div> <input className="form-control" type="text" placeholder="Link ToSource" {...linkToSource}/ > < /div></div >
+      < div className = "form-group" > < label > Contact Details < /label><div> <input className="form-control" type="text" placeholder="Contact Details" {...contactDetails}/ > < /div></div >
+      < div className = "form-group" >
+      < label > Tags < /label><div> <input className={`form-control ${tags.touched && tags.error ? ' has-danger' : ''}`} type="text"  placeholder="tags" {...tags}/ > < /div> < div className = "text-help" > {
+        tags.error
+      } < /div> < /div>
 
-<div>
-<button className={`btn btn-large btn-success has-spinner ${ {submitting} ? 'activee' : ''}`} type="submit" disabled={submitting} >
-<span className ="spinner"><i className="icon-spin icon-refresh"></i></span>  Submit
-</button>
+      < div >
+      < button className = {
+        `btn btn-large btn-success has-spinner ${ {submitting} ? 'activee' : ''}`
+      }
+      type = "submit"
+      disabled = {
+        submitting
+      } >
+      < span className = "spinner" > < i className = "icon-spin icon-refresh" > < /i></span > Submit < /button>
 
-<Link to="/" className="btn btn-danger"  type="button" >
-    Back
- </Link>
+      < Link to = "/"
+      className = "btn btn-danger"
+      type = "button" >
+      Back < /Link>
 
- <button className="btn btn-danger"  type="button" disabled={submitting} onClick={() => this.onDelete()} >
-  Delete
-  </button>
+      < button className = "btn btn-danger"
+      type = "button"
+      disabled = {
+        submitting
+      }
+      onClick = {
+        () => this.onDelete()
+      } >
+      Delete < /button>
 
 
-</div>
-      </form>
-      </div>
+      < /div> < /form> < /div>
     );
   }
 }
 
-function validate(values){
-const  errors = {};
+function validate(values) {
+  const errors = {};
 
-  if(!values.tags){
+  if (!values.tags) {
     errors.tags = 'please enter the tag values';
   }
   return errors;
 }
 
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   console.log(state.selectedAdmission);
   let selectedAdmission = state.selectedAdmission;
-  if(selectedAdmission.lastDate){
-      selectedAdmission.lastDate = selectedAdmission.lastDate.split('T')[0];
+  if (selectedAdmission.lastDate) {
+    selectedAdmission.lastDate = selectedAdmission.lastDate.split('T')[0];
   }
 
-  if(selectedAdmission.notificationDate){
-      selectedAdmission.notificationDate = selectedAdmission.notificationDate.split('T')[0];
+  if (selectedAdmission.notificationDate) {
+    selectedAdmission.notificationDate = selectedAdmission.notificationDate.split('T')[0];
   }
 
 
-  return {initialValues:state.selectedAdmission, _id:state.selectedAdmission._id};
+  return {
+    initialValues: state.selectedAdmission,
+    _id: state.selectedAdmission._id
+  };
 }
 
 export default reduxForm({
-  form:'admissionUpdateForm',
+  form: 'admissionUpdateForm',
   fields,
   validate
-},mapStateToProps,{updateAdmission, deleteAdmission})(UpdateAdmission);
+}, mapStateToProps, {
+  updateAdmission,
+  deleteAdmission
+})(UpdateAdmission);
